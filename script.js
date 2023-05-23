6263aa8c4941ee5411217a3d45c709dff2cc1248
Void-Reuel <127931828+Void-Reuel@users.noreply.github.com>
2023-05-23 11:23:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-document.getElementById("Err").style.display = "none";
-document.getElementById("sear").style.display = "none";
-document.getElementById("Canc").style.display = "none";
-document.getElementById("Ricerca").style.display = "none";
-    
-document.getElementById("backdoc").style.transform = "scale(0.00)";
-document.getElementById("doc").style.display = "none";
-document.getElementById("doc").style.transform = "scale(0.00)";
-document.getElementById("docu").style.display = "none";
+
 
 
 let MRIC;
@@ -333,7 +325,15 @@ function canc()
 
 function start()
 {
+    document.getElementById("Err").style.display = "none";
+    document.getElementById("sear").style.display = "none";
+    document.getElementById("Canc").style.display = "none";
+    document.getElementById("Ricerca").style.display = "none";
     
+    document.getElementById("backdoc").style.transform = "scale(0.00)";
+    document.getElementById("doc").style.display = "none";
+    document.getElementById("doc").style.transform = "scale(0.00)";
+    document.getElementById("docu").style.display = "none";
 }
 
 function not()
