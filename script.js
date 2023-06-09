38fa6566c29a696c7361a5b6d9bd3aee3743b143
Void-Reuel <127931828+Void-Reuel@users.noreply.github.com>
2023-06-09 09:15:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -409,7 +409,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+    if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110)
     {
         if (BarraR == 1){
             document.getElementById("backsearch").style.right = "90%";
